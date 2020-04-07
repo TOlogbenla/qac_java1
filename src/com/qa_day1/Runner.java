6c6510d5a11aa67ffd6c1d506ca0150382c370e1
Tinu <tinuade.ologbenla@academytrainee.com>
2020-04-07 16:37:22 +0100

    Adding files

---
+++ src/com/qa_day1/Runner.java
@@ -0,0 +1,28 @@
+package com.qa_day1;
+
+public class Runner {
+	// This is a single line comment
+	// control a selectects everything 
+	//
+
+	/*
+	 * This is a multi line comment
+	 */
+	public static void main(String[] args) {
+		int age = 23; // create variable age
+		// data type
+		// the name
+		// = to assign
+		// value
+		int height = 176;
+		String homeAdd = "124 example road, london m50 3sa";
+		Boolean bool = true;
+
+		System.out.println("Hello World!");
+		System.out.println("I am " + age);
+		System.out.println("I am " + height + "cm");
+		System.out.println(homeAdd);
+		System.out.println("The value of bool is " + bool);
+		System.out.println("My height is " + height + " my address is " + homeAdd + " the value of bool is " + bool);
+	}
+}
